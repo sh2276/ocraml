4e49b3a60af5b7932b654c70626f035f5c21c14c
Sahil Hosalli <sh2276@cornell.edu>
2023-11-14 10:12:51 -0500

    Add matrix test functions

--- test/main.ml
+++ test/main.ml
@@ -18,12 +18,15 @@ open OUnit2
 open Ocraml
 open Vector
 
-(*Tester functions*)
+(*==============================================================================
+                             VECTOR TEST SUITE
+==============================================================================*)
+(** Vector Test Functions *)
 
 (** Function to test if a vector was initialized correctly. *)
 let vector_init_tester (in1 : float array) =
-  let vect = init in1 in
-  let arr_from_vector = to_array vect in
+  let vect = Vector.init in1 in
+  let arr_from_vector = Vector.to_array vect in
   assert_equal
     ~printer:(fun arr ->
       Printf.sprintf "[|%s|]"
@@ -33,9 +36,9 @@ let vector_init_tester (in1 : float array) =
 (** Function to test vector add and subtract operations. *)
 let vector_a_s_tester (out : float array) (in1 : float array)
     (in2 : float array) (op : Vector.t -> Vector.t -> Vector.t) =
-  let v1 = init in1 in
-  let v2 = init in2 in
-  let arr_from_op = to_array (op v1 v2) in
+  let v1 = Vector.init in1 in
+  let v2 = Vector.init in2 in
+  let arr_from_op = Vector.to_array (op v1 v2) in
   assert_equal
     ~printer:(fun arr ->
       Printf.sprintf "[|%s|]"
@@ -44,9 +47,9 @@ let vector_a_s_tester (out : float array) (in1 : float array)
 
 (** Function to test vector scalar multiplication operation. *)
 let vector_s_mult_tester (out : float array) (in1 : float) (in2 : float array) =
-  let vect = init in2 in
-  let arr_from_op = to_array (scalar_mult in1 vect) in
-  let arr_from_infix = to_array (in1 * vect) in
+  let vect = Vector.init in2 in
+  let arr_from_op = Vector.to_array (scalar_mult in1 vect) in
+  let arr_from_infix = Vector.to_array Vector.(in1 * vect) in
   let ae arr =
     assert_equal
       ~printer:(fun arr ->
@@ -59,10 +62,10 @@ let vector_s_mult_tester (out : float array) (in1 : float) (in2 : float array) =
 
 (** Function to test vector dot product operation. *)
 let vector_dprod_tester (out : float) (in1 : float array) (in2 : float array) =
-  let v1 = init in1 in
-  let v2 = init in2 in
+  let v1 = Vector.init in1 in
+  let v2 = Vector.init in2 in
   let float_from_op = dot_prod v1 v2 in
-  let float_from_infix = v1 @ v2 in
+  let float_from_infix = Vector.(v1 @ v2) in
   let ae res =
     assert_equal ~printer:string_of_float ~msg:"Vector dot product failed." out
       res
@@ -72,8 +75,8 @@ let vector_dprod_tester (out : float) (in1 : float array) (in2 : float array) =
 
 (** Function to test the length of a vector. *)
 let vector_length_tester (in1 : float array) =
-  let vect = init in1 in
-  let vec_len = length vect in
+  let vect = Vector.init in1 in
+  let vec_len = Vector.length vect in
   let arr_len = Array.length in1 in
   assert_equal ~printer:string_of_int ~msg:"Vector length test failed." vec_len
     arr_len
@@ -155,168 +158,265 @@ let v20_2 =
     20.0;
   |]
 
-let tests =
-  "starter test suite"
-  >::: [
-         (*initialization tests*)
-         ( " initialize vector of length 5 with specific values" >:: fun _ ->
-           vector_init_tester v5 );
-         ( " initialize vector of length 10 with specific values" >:: fun _ ->
-           vector_init_tester v10 );
-         ( " initialize vector of length 15 with specific values" >:: fun _ ->
-           vector_init_tester v15 );
-         ( " initialize vector of length 20 with specific values" >:: fun _ ->
-           vector_init_tester v20 );
-         (*vector addition tests*)
-         ( " add two empty vectors" >:: fun _ ->
-           vector_a_s_tester [||] [||] [||] Vector.add );
-         ( " add two vectors of length 5" >:: fun _ ->
-           vector_a_s_tester [| 2.; 4.; 6.; 8.; 10. |] v5 v5 Vector.add );
-         ( " add two vectors of length 10" >:: fun _ ->
-           vector_a_s_tester
-             [| 0.2; 0.4; 0.6; 0.8; 1.; 1.2; 1.4; 1.6; 1.8; 2. |]
-             v10 v10 Vector.add );
-         ( " add two vectors of length 15" >:: fun _ ->
-           vector_a_s_tester
-             [|
-               -2.;
-               -4.;
-               -6.;
-               -8.;
-               -10.;
-               0.;
-               2.;
-               4.;
-               6.;
-               8.;
-               10.;
-               0.;
-               -2.;
-               -4.;
-               -6.;
-             |]
-             v15 v15 Vector.add );
-         ( " add two vectors of length 20" >:: fun _ ->
-           vector_a_s_tester
-             [|
-               5.;
-               7.;
-               9.;
-               11.;
-               13.;
-               15.;
-               17.;
-               19.;
-               21.;
-               23.;
-               25.;
-               27.;
-               29.;
-               31.;
-               33.;
-               35.;
-               37.;
-               39.;
-               41.;
-               43.;
-             |]
-             v20 v20 Vector.add );
-         ( " add two vectors of length 5 with different values" >:: fun _ ->
-           vector_a_s_tester [| 1.5; 3.5; 5.5; 7.5; 9.5 |] v5 v5_2 Vector.add );
-         (*vector subtraction tests*)
-         ( " subtract two empty vectors" >:: fun _ ->
-           vector_a_s_tester [||] [||] [||] Vector.sub );
-         ( " subtract two vectors of length 5" >:: fun _ ->
-           vector_a_s_tester [| 0.5; 0.5; 0.5; 0.5; 0.5 |] v5 v5_2 Vector.sub );
-         ( " subtract two vectors of length 10" >:: fun _ ->
-           vector_a_s_tester
-             [| -9.9; -8.8; -7.7; -6.6; -5.5; -4.4; -3.3; -2.2; -1.1; 0.0 |]
-             v10 v10_2 Vector.sub );
-         ( " subtract two vectors of length 15" >:: fun _ ->
-           vector_a_s_tester
-             [| 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0. |]
-             v15 v15 Vector.sub );
-         ( " subtract two vectors of length 20" >:: fun _ ->
-           vector_a_s_tester
-             [|
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-               1.5;
-             |]
-             v20 v20_2 Vector.sub );
-         ( " subtract two vectors of length 5 with different values" >:: fun _ ->
-           vector_a_s_tester
-             [| -0.5; -0.5; -0.5; -0.5; -0.5 |]
-             v5_2 v5 Vector.sub );
-         (*vector scalar multiplication tests*)
-         ( " multiply empty vector by 0.0" >:: fun _ ->
-           vector_s_mult_tester [||] 0.0 [||] );
-         ( " multiply vector of length 5 by 2.0" >:: fun _ ->
-           vector_s_mult_tester [| 2.0; 4.0; 6.0; 8.0; 10.0 |] 2.0 v5 );
-         ( " multiply vector of length 15 by 0.5" >:: fun _ ->
-           vector_s_mult_tester
-             [|
-               -0.5;
-               -1.0;
-               -1.5;
-               -2.0;
-               -2.5;
-               0.0;
-               0.5;
-               1.0;
-               1.5;
-               2.0;
-               2.5;
-               0.0;
-               -0.5;
-               -1.0;
-               -1.5;
-             |]
-             0.5 v15 );
-         (*vector dot product tests*)
-         ( " dot product of two singleton vectors" >:: fun _ ->
-           vector_dprod_tester 1.0 [| 1.0 |] [| 1.0 |] );
-         ( "dot product of two length 5 vectors" >:: fun _ ->
-           vector_dprod_tester 47.5 v5 v5_2 );
-         ( " dot product of two length 10 vectors" >:: fun _ ->
-           vector_dprod_tester 0.385 v10 v10_3 );
-         ( " dot product of two length 15 vectors" >:: fun _ ->
-           vector_dprod_tester 124.0 v15 v15 );
-         ( " dot product of two length 20 vectors" >:: fun _ ->
-           vector_dprod_tester 3185.0 v20 v20_2 );
-         (*vector length tests*)
-         (" length of empty vector" >:: fun _ -> vector_length_tester [||]);
-         (" length of vector of length 5" >:: fun _ -> vector_length_tester v5);
-         (" length of vector of length 10" >:: fun _ -> vector_length_tester v10);
-         (" length of vector of length 15" >:: fun _ -> vector_length_tester v15);
-         (" length of vector of length 20" >:: fun _ -> vector_length_tester v20);
-         ( " length of vector of length 5 with different values" >:: fun _ ->
-           vector_length_tester v5_2 );
-         ( " length of vector of length 10 with different values" >:: fun _ ->
-           vector_length_tester v10_2 );
-         ( " length of vector of length 20 with different values" >:: fun _ ->
-           vector_length_tester v20_2 );
-         ( " length of vector of length 5 with negative values" >:: fun _ ->
-           vector_length_tester v5_3 );
-         ( " length of vector of length 10 with negative values" >:: fun _ ->
-           vector_length_tester v10_3 );
-       ]
+(** Vector test cases*)
+let vec_tests =
+  [
+    (*initialization tests*)
+    ( " initialize vector of length 5 with specific values" >:: fun _ ->
+      vector_init_tester v5 );
+    ( " initialize vector of length 10 with specific values" >:: fun _ ->
+      vector_init_tester v10 );
+    ( " initialize vector of length 15 with specific values" >:: fun _ ->
+      vector_init_tester v15 );
+    ( " initialize vector of length 20 with specific values" >:: fun _ ->
+      vector_init_tester v20 );
+    (*vector addition tests*)
+    ( " add two empty vectors" >:: fun _ ->
+      vector_a_s_tester [||] [||] [||] Vector.add );
+    ( " add two vectors of length 5" >:: fun _ ->
+      vector_a_s_tester [| 2.; 4.; 6.; 8.; 10. |] v5 v5 Vector.add );
+    ( " add two vectors of length 10" >:: fun _ ->
+      vector_a_s_tester
+        [| 0.2; 0.4; 0.6; 0.8; 1.; 1.2; 1.4; 1.6; 1.8; 2. |]
+        v10 v10 Vector.add );
+    ( " add two vectors of length 15" >:: fun _ ->
+      vector_a_s_tester
+        [|
+          -2.; -4.; -6.; -8.; -10.; 0.; 2.; 4.; 6.; 8.; 10.; 0.; -2.; -4.; -6.;
+        |]
+        v15 v15 Vector.add );
+    ( " add two vectors of length 20" >:: fun _ ->
+      vector_a_s_tester
+        [|
+          5.;
+          7.;
+          9.;
+          11.;
+          13.;
+          15.;
+          17.;
+          19.;
+          21.;
+          23.;
+          25.;
+          27.;
+          29.;
+          31.;
+          33.;
+          35.;
+          37.;
+          39.;
+          41.;
+          43.;
+        |]
+        v20 v20 Vector.add );
+    ( " add two vectors of length 5 with different values" >:: fun _ ->
+      vector_a_s_tester [| 1.5; 3.5; 5.5; 7.5; 9.5 |] v5 v5_2 Vector.add );
+    (*vector subtraction tests*)
+    ( " subtract two empty vectors" >:: fun _ ->
+      vector_a_s_tester [||] [||] [||] Vector.sub );
+    ( " subtract two vectors of length 5" >:: fun _ ->
+      vector_a_s_tester [| 0.5; 0.5; 0.5; 0.5; 0.5 |] v5 v5_2 Vector.sub );
+    ( " subtract two vectors of length 10" >:: fun _ ->
+      vector_a_s_tester
+        [| -9.9; -8.8; -7.7; -6.6; -5.5; -4.4; -3.3; -2.2; -1.1; 0.0 |]
+        v10 v10_2 Vector.sub );
+    ( " subtract two vectors of length 15" >:: fun _ ->
+      vector_a_s_tester
+        [| 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0. |]
+        v15 v15 Vector.sub );
+    ( " subtract two vectors of length 20" >:: fun _ ->
+      vector_a_s_tester
+        [|
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+          1.5;
+        |]
+        v20 v20_2 Vector.sub );
+    ( " subtract two vectors of length 5 with different values" >:: fun _ ->
+      vector_a_s_tester [| -0.5; -0.5; -0.5; -0.5; -0.5 |] v5_2 v5 Vector.sub );
+    (*vector scalar multiplication tests*)
+    ( " multiply empty vector by 0.0" >:: fun _ ->
+      vector_s_mult_tester [||] 0.0 [||] );
+    ( " multiply vector of length 5 by 2.0" >:: fun _ ->
+      vector_s_mult_tester [| 2.0; 4.0; 6.0; 8.0; 10.0 |] 2.0 v5 );
+    ( " multiply vector of length 15 by 0.5" >:: fun _ ->
+      vector_s_mult_tester
+        [|
+          -0.5;
+          -1.0;
+          -1.5;
+          -2.0;
+          -2.5;
+          0.0;
+          0.5;
+          1.0;
+          1.5;
+          2.0;
+          2.5;
+          0.0;
+          -0.5;
+          -1.0;
+          -1.5;
+        |]
+        0.5 v15 );
+    (*vector dot product tests*)
+    ( " dot product of two singleton vectors" >:: fun _ ->
+      vector_dprod_tester 1.0 [| 1.0 |] [| 1.0 |] );
+    ( "dot product of two length 5 vectors" >:: fun _ ->
+      vector_dprod_tester 47.5 v5 v5_2 );
+    ( " dot product of two length 10 vectors" >:: fun _ ->
+      vector_dprod_tester 0.385 v10 v10_3 );
+    ( " dot product of two length 15 vectors" >:: fun _ ->
+      vector_dprod_tester 124.0 v15 v15 );
+    ( " dot product of two length 20 vectors" >:: fun _ ->
+      vector_dprod_tester 3185.0 v20 v20_2 );
+    (*vector length tests*)
+    (" length of empty vector" >:: fun _ -> vector_length_tester [||]);
+    (" length of vector of length 5" >:: fun _ -> vector_length_tester v5);
+    (" length of vector of length 10" >:: fun _ -> vector_length_tester v10);
+    (" length of vector of length 15" >:: fun _ -> vector_length_tester v15);
+    (" length of vector of length 20" >:: fun _ -> vector_length_tester v20);
+    ( " length of vector of length 5 with different values" >:: fun _ ->
+      vector_length_tester v5_2 );
+    ( " length of vector of length 10 with different values" >:: fun _ ->
+      vector_length_tester v10_2 );
+    ( " length of vector of length 20 with different values" >:: fun _ ->
+      vector_length_tester v20_2 );
+    ( " length of vector of length 5 with negative values" >:: fun _ ->
+      vector_length_tester v5_3 );
+    ( " length of vector of length 10 with negative values" >:: fun _ ->
+      vector_length_tester v10_3 );
+  ]
 
-let _ = run_test_tt_main tests
+(*==============================================================================
+                             MATRIX TEST SUITE
+==============================================================================*)
+
+(**Matrix test functions*)
+
+(** Function to convert a row of a matrix to a string. *)
+let row_to_string row =
+  String.concat "; " Array.(to_list (map string_of_float row))
+
+(** Function to test correct initialization of the matrix. *)
+let matrix_init_tester (in1 : float array array) =
+  let mat = Matrix.init in1 in
+  let arr_from_matrix = Matrix.to_array mat in
+  assert_equal
+    ~printer:(fun mat ->
+      Printf.sprintf "[|%s|]"
+        (String.concat "; " (Array.to_list (Array.map row_to_string mat))))
+    ~msg:"Matrix not initialized properly." in1 arr_from_matrix
+
+(** Function to test num_rows and num_cols simultaneously. *)
+let matrix_dim_tester (mat : float array array) (r : int) (c : int) =
+  let mat = Matrix.init mat in
+  let actual_rows = Matrix.num_rows mat in
+  let actual_cols = Matrix.num_cols mat in
+  assert_equal ~printer:string_of_int ~msg:"Number of rows mismatch." r
+    actual_rows;
+  assert_equal ~printer:string_of_int ~msg:"Number of columns mismatch." c
+    actual_cols
+
+(** Function to test matrix-vector multiplication. *)
+let mat_vec_prod_tester (out : float array) (mat : float array array)
+    (vec : float array) =
+  let mat = Matrix.init mat in
+  let vec = Vector.init vec in
+  let prod = Vector.to_array (Matrix.mat_vec_prod mat vec) in
+  let prod_2 = Vector.to_array Matrix.(mat @ vec) in
+  let ae o i =
+    assert_equal
+      ~printer:(fun arr ->
+        Printf.sprintf "[|%s|]"
+          (String.concat "; " Array.(to_list (map string_of_float arr))))
+      ~msg:"Vector operation failed." o i
+  in
+  ae out prod;
+  ae out prod_2
+
+(** Function to test matrix-matrix multiplication. *)
+let mat_mat_prod_tester (out : float array array) (mat1 : float array array)
+    (mat2 : float array array) =
+  let mat1 = Matrix.init mat1 in
+  let mat2 = Matrix.init mat2 in
+  let prod = Matrix.to_array (Matrix.mat_mat_prod mat1 mat2) in
+  let prod_2 = Matrix.to_array Matrix.(mat1 * mat2) in
+  let ae o i =
+    assert_equal
+      ~printer:(fun m ->
+        Printf.sprintf "[|%s|]"
+          (String.concat "; " (Array.to_list (Array.map row_to_string m))))
+      ~msg:"Matrix operation failed." o i
+  in
+  ae out prod;
+  ae out prod_2
+
+(** Function to test matrix transpose. *)
+let matrix_transpose_tester (mat : float array array) (out : float array array)
+    =
+  let mat = Matrix.init mat in
+  let trans = Matrix.to_array (Matrix.transpose mat) in
+  let ae o i =
+    assert_equal
+      ~printer:(fun m ->
+        Printf.sprintf "[|%s|]"
+          (String.concat "; " (Array.to_list (Array.map row_to_string m))))
+      ~msg:"Matrix transpose failed." o i
+  in
+  ae out trans
+
+(* Test cases for the Matrix module *)
+let m3_3 = [| [| 1.0; 2.0; 3.0 |]; [| 4.0; 5.0; 6.0 |]; [| 7.0; 8.0; 9.0 |] |]
+let m3_3_2 = [| [| 2.0; 0.0; 1.0 |]; [| 1.0; 0.0; 1.0 |]; [| 1.0; 1.0; 0.0 |] |]
+let v3 = [| 1.0; 2.0; 3.0 |]
+
+(**Matrix Test Cases*)
+let mat_tests =
+  [
+    (* Initialization tests *)
+    ("initialize empty matrix" >:: fun _ -> matrix_init_tester [||]);
+    ( "initialize matrix with specific values" >:: fun _ ->
+      matrix_init_tester m3_3 );
+    (* Matrix dimensions test *)
+    ("check matrix dimensions" >:: fun _ -> matrix_dim_tester m3_3 3 3);
+    (* Matrix-vector multiplication tests *)
+    ( "multiply matrix by vector" >:: fun _ ->
+      mat_vec_prod_tester [| 14.0; 32.0; 50.0 |] m3_3 v3 );
+    ( "multiply matrix by vector using infix operator" >:: fun _ ->
+      mat_vec_prod_tester [| 14.0; 32.0; 50.0 |] m3_3 v3 );
+    (* Matrix-matrix multiplication tests *)
+    ( "multiply two matrices" >:: fun _ ->
+      mat_mat_prod_tester
+        [|
+          [| 7.0; 3.0; 3.0 |]; [| 19.0; 6.0; 9.0 |]; [| 31.0; 9.0; 15.0 |];
+        |]
+        m3_3 m3_3_2 );
+    (* Matrix transpose test *)
+    ( "transpose matrix" >:: fun _ ->
+      matrix_transpose_tester m3_3
+        [| [| 1.0; 4.0; 7.0 |]; [| 2.0; 5.0; 8.0 |]; [| 3.0; 6.0; 9.0 |] |] );
+    (* Conversion to array test *)
+    ("convert matrix to array" >:: fun _ -> matrix_init_tester m3_3);
+  ]
+
+let suite = "ocraml test suite" >::: List.flatten [ vec_tests; mat_tests ]
+let _ = run_test_tt_main suite
