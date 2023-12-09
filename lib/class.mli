2dbf6324785f97a29502f7363dd123a28a3b219b
Farhan Naqib <fn72@cornell.edu>
2023-12-09 05:29:48 -0500

    Add basic setup for Perceptron functor

---
+++ lib/class.mli
@@ -0,0 +1,4 @@
+(** Module to represent a classification *)
+
+type 'a t
+(** Representation type of a [Class] *)
--- lib/perceptron.mli
+++ lib/perceptron.mli
@@ -1,14 +1,16 @@
 (** Module to represent a perceptron and its operations. *)
 
-type t
+type 'a t
 (** Representation type of [Perceptron] *)
 
-val create : int -> int -> t
-(** [create size num_classes] creates a new perceptron with [size] inputs *)
+val create : int -> int -> (int -> 'a) -> 'a t
+(** [create n c map] creates a new perceptron with [n] inputs, with [c] classes. 
+    Requires: [map] is a map from the integers [0, c - 1] to the set of specified results. *)
 
-val predict : Vector.t -> t -> int
+val predict : Vector.t -> 'a t -> 'a
 (** [predict inputs perceptron] makes a prediction based on the inputs. 
     Requires: [inputs] has a dimension of nx1 and [perceptron] was created with size n. *)
 
-val update_weights : float -> int -> Vector.t -> t -> t
-(** [update_weights learning_rate expected actual inputs perceptron] updates the weights of the perceptron based on the error. *)
+val update_weights : float -> 'a -> Vector.t -> 'a t -> 'a t
+(** [update_weights learning_rate expected actual inputs perceptron] updates the weights of the 
+    perceptron based on the error. *)
