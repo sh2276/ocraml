6c42c3e6382f05aa2e501a2b8d5fc743242b3e48
Farhan Naqib <fn72@cornell.edu>
2023-12-09 13:35:33 -0500

    Make Perceptron into a functor

--- lib/perceptron.ml
+++ lib/perceptron.ml
@@ -1,27 +1,72 @@
-type 'a t = Matrix.t * Vector.t * (int -> 'a)
-(* AF: A perceptron [(m, b, f)] is represented by a tuple of a vector [m], a
-   float representing bias [b], and binding function [f] that maps the integers
-   [0, c - 1] to a set of results. RI: abs(b) < 1 *)
+module type ClassType = sig
+  type 'a t
 
-let create num_inputs num_classes map =
-  let weights = Matrix.init (Array.make_matrix num_classes num_inputs 0.0) in
-  let biases = Vector.init (Array.make num_classes 0.0) in
-  (weights, biases, map)
+  val create : (int * 'a) list -> 'a t
+  val get : 'a t -> int -> 'a
+  val index : 'a t -> 'a -> int
+  val num_of_classes : 'a t -> int
+end
 
-let predict inputs (m, b, f) =
-  let open Matrix in
-  assert_m_v_dim m inputs;
-  let unbiased = m @ inputs in
-  let open Vector in
-  unbiased + b |> argmax |> f
+module Class : ClassType = struct
+  type 'a t = (int * 'a) list
+  (** AF: Association list of keys and classes. RI: The map resulting from the
+      association list must be one-to-one. The set of keys must be from
+      [0, n-1]. *)
 
-let update_weights _ _ _ _ = failwith "amogus"
+  let create lst = lst
+  let get lst n = List.assoc n lst
+  let index lst n = List.find (fun p -> snd p = n) lst |> fst
+  let num_of_classes lst = List.length lst
+end
 
-(* let update_weights learning_rate expected inputs (m, b, f) = let actual =
-   predict inputs (m, b, f) in if expected = actual then (m, b, f) else let
-   target = Array.make (Vector.length b) 0.0 in target.(actual) <- 1.0;
-   target.(expected) <- -1.0; let target_v = Vector.init target in
+module type Perceptron = sig
+  module C : ClassType
 
-   let error = Vector.(target_v - b) in let add_w = Matrix.( ([| Vector.to_array
-   error |] |> init |> transpose) * init [| Vector.(b |> ( * ) learning_rate |>
-   to_array) |]) in (Matrix.(m + add_w), Vector.(b + error), f) *)
+  type 'a return = 'a C.t
+  type 'a t
+
+  val create : int -> (int * 'a) list -> 'a t
+  val predict : Vector.t -> 'a t -> 'a
+  val update_weights : float -> 'a -> Vector.t -> 'a t -> 'a t
+end
+
+module PerceptronBuilder (C : ClassType) : Perceptron with module C = C = struct
+  module C = C
+
+  type 'a return = 'a C.t
+  type 'a t = Matrix.t * Vector.t * 'a return
+  (* AF: A perceptron [(m, b, f)] is represented by a tuple of a vector [m], a
+     float representing bias [b], and binding function [f] that maps the
+     integers [0, c - 1] to a set of results. RI: abs(b) < 1 *)
+
+  let create num_inputs lst =
+    let c = C.create lst in
+    let num_classes = C.num_of_classes c in
+    let weights = Matrix.init (Array.make_matrix num_classes num_inputs 0.0) in
+    let biases = Vector.init (Array.make num_classes 0.0) in
+    (weights, biases, c)
+
+  let predict inputs (m, b, f) =
+    let open Matrix in
+    assert_m_v_dim m inputs;
+    let unbiased = m @ inputs in
+    let open Vector in
+    unbiased + b |> argmax |> C.get f
+
+  let update_weights learning_rate expected inputs (m, b, f) =
+    let actual = predict inputs (m, b, f) in
+    if expected = actual then (m, b, f)
+    else
+      let target = Array.make (Vector.length b) 0.0 in
+      target.(C.index f actual) <- 1.0;
+      target.(C.index f expected) <- -1.0;
+      let target_v = Vector.init target in
+
+      let error = Vector.(target_v - b) in
+      let add_w =
+        Matrix.(
+          ([| Vector.to_array error |] |> init |> transpose)
+          * init [| Vector.(b |> ( * ) learning_rate |> to_array) |])
+      in
+      (Matrix.(m + add_w), Vector.(b + error), f)
+end
--- lib/perceptron.mli
+++ lib/perceptron.mli
@@ -1,16 +1,46 @@
 (** Module to represent a perceptron and its operations. *)
 
-type 'a t
-(** Representation type of [Perceptron] *)
+module type ClassType = sig
+    type 'a t
+    (** Representation type of a [Class] *)
+  
+    val create : (int * 'a) list -> 'a t
+    (** [create lst] creates a new [Class] with the bindings present in the
+        association list. Requires: [lst] must be a one-to-one mapping from each
+        int to each result. The set of keys must be [0, ..., n-1] where n is the
+        number of distinct results. *)
+  
+    val get : 'a t -> int -> 'a
+    (** [get classes n] will return the binding with [n] as the key. *)
+  
+    val index : 'a t -> 'a -> int
+    (** [index classes r] will return the binding with [r] as the result. *)
 
-val create : int -> int -> (int -> 'a) -> 'a t
-(** [create n c map] creates a new perceptron with [n] inputs, with [c] classes. 
-    Requires: [map] is a map from the integers [0, c - 1] to the set of specified results. *)
+    val num_of_classes : 'a t -> int
+    (** [num_of_classes] will return the number of classes in a [Class]. *)
+  end
 
-val predict : Vector.t -> 'a t -> 'a
-(** [predict inputs perceptron] makes a prediction based on the inputs. 
-    Requires: [inputs] has a dimension of nx1 and [perceptron] was created with size n. *)
+module Class : ClassType
 
-val update_weights : float -> 'a -> Vector.t -> 'a t -> 'a t
-(** [update_weights learning_rate expected actual inputs perceptron] updates the weights of the 
-    perceptron based on the error. *)
+module type Perceptron = sig
+    module C : ClassType
+
+    type 'a return = 'a C.t
+    (** [return] is the type of the class. *)
+
+    type 'a t
+    (** Representation type of [Perceptron] *)
+
+    val create : int -> (int * 'a) list -> 'a t
+    (** *)
+    
+    val predict : Vector.t -> 'a t -> 'a
+    (** [predict inputs perceptron] makes a prediction based on the inputs. 
+        Requires: [inputs] has a dimension of nx1 and [perceptron] was created with size n. *)
+    
+    val update_weights : float -> 'a -> Vector.t -> 'a t -> 'a t
+    (** [update_weights learning_rate expected actual inputs perceptron] updates the weights of the 
+        perceptron based on the error. *)
+end
+
+module PerceptronBuilder (C : ClassType) : Perceptron with module C = C
